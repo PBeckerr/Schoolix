974467c680ada9f8b9c3dc1090a250d146cd82c8
Paul Becker <paul.becker@develappers.de>
2020-03-20 20:31:40 +0100

    Init#2

---
+++ CoronaApi/CoronaApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoronaApi.BackgroundServices.Channels;
+using CoronaApi.MediatR.Commands;
+using CoronaApi.MediatR.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoronaApi.Controllers
+{
+    public class WeatherForecastController : BaseV1ApiController
+    {
+        private readonly FileProcessingChannel _fileProcessingChannel;
+        private readonly IMediator _mediator;
+
+        public WeatherForecastController(IMediator mediator, FileProcessingChannel fileProcessingChannel)
+        {
+            this._fileProcessingChannel = fileProcessingChannel;
+            this._mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get([FromQuery]GetAllWeatherForecastsQuery query)
+        {
+            await this._fileProcessingChannel.AddFileAsync(Guid.NewGuid().ToString());
+            return await this._mediator.Send(query)
+                             .ConfigureAwait(false);
+        }
+
+        [HttpPost]
+        public async Task<WeatherForecast> Post(CreateWeathterForecastCommand command)
+        {
+            return await this._mediator.Send(command)
+                             .ConfigureAwait(false);
+        }
+    }
+}
---
+++ CoronaApi/CoronaApi/Startup.cs
@@ -0,0 +1,103 @@
+using CoronaApi.Core;
+using CoronaApi.MediatR;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace CoronaApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //might be not desirable depending on project
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                                  builder => builder.AllowAnyOrigin()
+                                                    .AllowAnyMethod()
+                                                    .AllowAnyHeader());
+            });
+
+            services.AddApplication();
+
+            services.AddResponseCompression();
+            services.AddResponseCaching();
+
+            services.AddControllers()
+                    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>())
+                    .AddNewtonsoftJson(options => { options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; });
+
+            // Customise default API behaviour
+            services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+            app.UseCustomExceptionHandler();
+            app.UseAuthorization();
+            app.UseResponseCompression();
+            app.UseResponseCaching();
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
