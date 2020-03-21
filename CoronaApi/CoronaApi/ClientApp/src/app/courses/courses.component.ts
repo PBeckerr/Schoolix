c859834d4f5fe51c900022d6b1a61d17eadb7187
Jakob Neumann <jakob@polarfox.dev>
2020-03-21 18:48:30 +0100

    hide teacher name for teacher

--- CoronaApi/CoronaApi/ClientApp/src/app/courses/courses.component.ts
+++ CoronaApi/CoronaApi/ClientApp/src/app/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Course } from '../core/models/course';
 import { Router } from '@angular/router';
+import { UserService } from '../core/services/user/user.service';
+import { UserType } from '../core/models/user';
 
 @Component({
   selector: 'app-courses',
@@ -10,10 +12,15 @@ import { Router } from '@angular/router';
 export class CoursesComponent implements OnInit {
 
   @Input() courses: Course[];
+  userType: UserType;
+  UserType = UserType;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private userService: UserService
+  ) {
+    this.userType = userService.getCurrentUser().userType;
+  }
 
   ngOnInit() {
   }
