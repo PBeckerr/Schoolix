9246c4c5bc3a52d4e554762fb6734ef532b9ec6c
Paul Becker <paul.becker@develappers.de>
2020-03-22 09:33:20 +0100

    Sample with real username in menu

--- CoronaApi/CoronaApi/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ CoronaApi/CoronaApi/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { User } from '../core/models/user';
 import { UserService } from '../core/services/user/user.service';
+import {AuthorizeService} from '../../api-authorization/authorize.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
 
   user: User;
+  private userName: string;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private authorizeService: AuthorizeService,
   ) {
     this.user = this.userService.getCurrentUser();
   }
 
+  ngOnInit() {
+    this.authorizeService.getUser().subscribe(value => {
+      this.userName = value.userName;
+    });
+  }
 }
