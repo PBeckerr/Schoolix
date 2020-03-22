bfd1f66f7ab8baff3cc3afa3f1748a5172169857
Paul Becker <paul.becker@develappers.de>
2020-03-22 11:09:56 +0100

    Translation

--- CoronaApi/CoronaApi/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CoronaApi/CoronaApi/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -55,21 +55,22 @@ namespace CoronaApi.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "E-Mail")]
             public string Email { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Passwort")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Passwort bestätigen")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
             [Required]
+            [Display(Name ="Typ")]
             public UserType UserType { get; set; }
         }
 
